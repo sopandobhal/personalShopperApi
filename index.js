3bf3d6c48fe373aed17920a4351ee56f64c27f2e
Sopan Dobhal <sopandobhal@Sopans-MacBook-Air.local>
2017-03-19 02:22:14 -0500

    Implimented delete

--- index.js
+++ index.js
@@ -17,20 +17,50 @@ function addNewItem(item){
 	grocery[numOfItems] = item;
 }
 
+function getDeleteableItem(id) {
+	var deleteableItem=null;
+	for (var i = grocery.length - 1; i >= 0; i--) {
+		if(grocery[i].id == id)
+			deleteableItem = grocery[i];
+	}
+	return deleteableItem;
+}
+
+function removeItem(item) {
+	var index = grocery.indexOf(item);
+
+	if (index>-1)
+       grocery.splice(index,1);
+}
+
 app.get("/grocery", function (req,res) {
+	console.log("GET REQUEST");
+	console.log(grocery);
 	res.json(grocery)
 });
 
 app.post("/grocery", function (req,res) {
-	// if (req.body.list == null) 
- //      res.json({"post":"success"})	
- //    else
- //    	res.json(req.body)
      var item = new createNewItem(req.body.item.name, req.body.item.qty);
      addNewItem(item);
+     console.log("POST REQUEST");
+     console.log(item);
      res.json(item);
 });
 
-app.delete("/grocery/")
+app.delete("/grocery/:id", function function_name(req, res) {
+	var id = req.params.id;
+	var item = getDeleteableItem(id);
+	console.log("DELETE REQUEST");
+
+	if (item == null){
+		console.log("Item not found!!");
+		res.send("Item not found!!");
+	}
+	else{
+		removeItem(item);
+		console.log(item);
+        res.json(item);
+	}
+});
 
 app.listen(3000);
